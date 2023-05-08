acaa551a2bac4dec80b6220756efb13d888cd243
Omar Merino <omarmerino9@gmail.com>
2023-05-07 21:48:07 -0400

    agregados crud
    get,add,subir imagen,delete de "agregados"

--- src/app.js
+++ src/app.js
@@ -178,3 +178,165 @@ app.put('/upload/producto/:nombre',async (req,res)=>{
     });
     
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//agregados
+
+//get agregadoss firebase
+app.get('/getAgregados', async (req, res) => {
+    try {
+      const snapshot = await db.collection('agregados').get();
+      const documentos = snapshot.docs.map(doc => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        };
+      });
+      res.send(documentos);
+      console.log(documentos);
+    } catch (error) {
+      console.log('Error obteniendo los documentos:', error);
+      res.status(500).send('Error obteniendo los documentos');
+    }
+});
+  
+
+//añadir agregados 
+app.post('/addAgregados', async (req, res) => {
+    const data = req.body;
+    console.log(data)
+    const docRef = await db.collection('agregados').add(data);
+    res.send(`Documento agregado con ID ${docRef.id}.`);
+});
+
+
+
+// Definimos la ruta para eliminar un agregados por su ID
+app.delete('/agregados/:id', async (req, res) => {
+    try {
+      const id = req.params.id;
+      // Creamos una referencia a la colección de agregados en Firestore
+      const agregadosRef = admin.firestore().collection('agregados');
+      
+      // Creamos una referencia al documento del producto que deseamos eliminar
+      const productoRef = agregadosRef.doc(id);
+  
+      // Obtenemos el documento actual
+      const doc = await productoRef.get();
+  
+      // Si el documento no existe, retornamos un error 404
+      if (!doc.exists) {
+        res.status(404).send('No se encontró el producto');
+      } else {
+        // Si el documento existe, lo eliminamos
+        await productoRef.delete();
+        res.send('Producto eliminado');
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Ocurrió un error al eliminar el producto');
+    }
+});
+  
+
+  
+
+//subir imagen indicando el id del producto y pasando una imagen 
+app.put('/upload/producto/:nombre',async (req,res)=>{
+    let nombre = req.params.nombre;  
+    if(!req.files){
+        return res.status(400).json({
+            ok:false,
+            mensaje: 'no selecciono nada',
+            errors: {message: 'debe de selccionar una imagen'}
+        });
+    }
+    
+
+    //obtener nombre del archivo
+
+    let archivo = req.files.imagen;
+
+    let nombreCortado = archivo.name.split('.');
+
+    let extensionArchivo = nombreCortado[nombreCortado.length - 1]
+
+    //extenciones que se aceptan
+    let extencionesValidas = ['png','jpg','gif','jpeg'];
+
+    if(extencionesValidas.indexOf(extensionArchivo)< 0){
+        return res.status (400).json({
+            ok:false,
+            mensaje: 'Extencion no valida',
+            errors: {message: 'las extenciones validas son:'+ extencionesValidas.join(', ')}
+
+        });
+    }
+
+    let nombreArchivo = `${nombre}.${extensionArchivo}`;
+    let path= `./uploads/${nombreArchivo}` ;
+    console.log(path);
+    
+
+    archivo.mv(path, err=>{
+        if (err){
+            return res.status(500).json({
+                ok:false,
+                mensaje: 'Error al mover archivo',
+                errors: err
+            });
+        }
+
+        return res.status(200).json({
+            ok:true,
+            mensaje:'peticion realizada correctamente'
+        });
+    })
+
+    // Creamos una referencia a la colección de agregados en Firestore
+    const agregadosRef = admin.firestore().collection('agregados');
+    // Filtrar documentos donde el campo "nombre" sea igual al nombre proporcionado
+    const query = agregadosRef.where('nombre', '==', nombre);
+    // Obtener los documentos que cumplen con el filtro
+    query.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        // Establecer el nuevo dato en cada documento
+        const docRef = agregadosRef.doc(doc.id);
+        docRef.update({ imagen: nombreArchivo })
+        .then(() => {
+            console.log("Dato actualizado en el documento:", doc.id);
+        })
+        .catch((error) => {
+            console.log("Error al actualizar el dato en el documento:", doc.id, error);
+        });
+    });
+    }).catch((error) => {
+    console.log("Error al obtener los documentos:", error);
+    });
+    
+});
